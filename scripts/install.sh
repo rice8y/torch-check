11c872e32adc05bf45d389bf7db685581b49c7b4
rice8y <eitoy1128@mbr.nifty.com>
2026-08-24 18:13:37 +0900

    chore: bump version to 0.1.2

--- scripts/install.sh
+++ scripts/install.sh
@@ -23,7 +23,7 @@ Usage:
   install.sh [OPTIONS]
 
 Options:
-  --version <VERSION>      Install a release such as 0.1.1 or v0.1.1
+  --version <VERSION>      Install a release such as 0.1.2 or v0.1.2
                            (default: latest)
   --install-dir <DIR>      Install into DIR (default: $HOME/.local/bin)
   -q, --quiet              Suppress informational output
